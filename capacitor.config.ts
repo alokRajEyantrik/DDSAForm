7b829310041dbc5e983f7a41289e72782c51b44b
alokRajEyantrik <alok.kumar@eyantrik.com>
2025-07-19 17:04:51 +0530

    feat: add server configuration to capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,10 @@ const config: CapacitorConfig = {
   appId: 'com.example.ddsaform',
   appName: 'ddsaform',
   webDir: 'build',
+  server: {
+    url: 'http://192.168.1.5:8000',
+    cleartext: true
+  }
 };
 
 export default config;
