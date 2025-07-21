9208c9fb11211b5fe7827530a95413594a91e782
alokRajEyantrik <alok.kumar@eyantrik.com>
2025-07-21 13:36:24 +0530

    fix: comment out server configuration in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,11 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.example.ddsaform',
   appName: 'ddsaform',
   webDir: 'build',
-  // server: {
-  //   url: 'http://10.0.2.2:8000',
-  //   cleartext: true,
-  //   allowNavigation: ['ddsa-api-1.onrender.com']
-  // }
 };
 
 export default config;
